529a51880d53645fe00166ac998d6e9a2e835402
ll17z2d <ll17z2d@leeds.ac.uk>
2022-06-03 13:50:44 +0100

    Changed caret colour and preliminary textbox shape work implemented

---
+++ DND Character Sheet/User Controls/Testing TextBox Outlines.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DND_Character_Sheet.User_Controls
+{
+    /// <summary>
+    /// Interaction logic for Testing_TextBox_Outlines.xaml
+    /// </summary>
+    public partial class Testing_TextBox_Outlines : UserControl
+    {
+        public Testing_TextBox_Outlines()
+        {
+            InitializeComponent();
+        }
+    }
+}
